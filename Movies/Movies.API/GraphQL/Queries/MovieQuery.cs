285d7ad144cfb4a3b3789b801904c8eca6247f5e
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-02-24 16:06:44 -0300

    Create MovieQuery.cs

---
+++ Movies/Movies.API/GraphQL/Queries/MovieQuery.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+using Movies.API.GraphQL.Types;
+
+namespace Movies.API.GraphQL.Queries
+{
+    public class MovieQuery : ObjectGraphType
+    {
+        public MovieQuery(MoviesDbContext db)
+        {
+            Field<ListGraphType<MovieType>>("movies").ResolveAsync(async a=> await db.Movies.ToListAsync());
+        }
+    }
+}
