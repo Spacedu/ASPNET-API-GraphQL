9c872202df2fcd4843d54a1918e0ba65567d331b
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-02-23 23:04:57 -0300

    S04E04 - Criar DbContext

---
+++ Movies/Movies.API/Data/MoviesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Models;
+
+namespace Movies.API.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieReview> Reviews { get; set; }
+
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().Property(e => e.Genre).HasConversion<string>();
+        }
+    }
+}
