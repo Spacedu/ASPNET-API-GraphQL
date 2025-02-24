209ac2450698bc0f71ebf3678e45fedb14fe286a
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-02-24 16:17:33 -0300

    S05E07 - Criar Consulta - Parte 1 - Buscar um Filme

--- Movies/Movies.API/GraphQL/Queries/MovieQuery.cs
+++ Movies/Movies.API/GraphQL/Queries/MovieQuery.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GraphQL;
+using Microsoft.EntityFrameworkCore;
 using Movies.API.Data;
 using Movies.API.GraphQL.Types;
 
@@ -8,7 +9,16 @@ namespace Movies.API.GraphQL.Queries
     {
         public MovieQuery(MoviesDbContext db)
         {
-            Field<ListGraphType<MovieType>>("movies").ResolveAsync(async a=> await db.Movies.ToListAsync());
+            Field<ListGraphType<MovieType>>("movies")
+                .ResolveAsync(async a=> await db.Movies.ToListAsync());
+
+            Field<MovieType>("movie")
+                .Arguments(new QueryArguments(new QueryArgument<NonNullGraphType<IntGraphType>>() { Name = "id" }))
+                .ResolveAsync(async context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return await db.Movies.SingleAsync(a=>a.Id == id);
+                });                
         }
     }
 }
