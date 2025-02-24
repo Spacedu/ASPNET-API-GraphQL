137b2ba4b31c07ba667e89ab19c08f3a62ecfe19
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-02-23 22:45:23 -0300

    S04E02 - Criar Modelos

---
+++ Movies/Movies.API/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Movies.API.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime LaunchDate { get; set; }
+
+        public ICollection<MovieReview>? Reviews { get; set; }
+    }
+}
---
+++ Movies/Movies.API/Models/MovieReview.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies.API.Models
+{
+    public class MovieReview
+    {
+        public int Id { get; set; }
+        public int Rate { get; set; }
+        public string Comment { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+    }
+}
