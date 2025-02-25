2f0a33965616ce0fa9b231992723e65e48342a51
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-02-24 23:23:32 -0300

    S05E10 - Resolve

--- Movies/Movies.API/GraphQL/Types/MovieType.cs
+++ Movies/Movies.API/GraphQL/Types/MovieType.cs
@@ -1,18 +1,24 @@
-﻿using Movies.API.GraphQL.Types.Enums;
-using Ext = GraphQL.Types;
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+using Movies.API.GraphQL.Types.Enums;
 
 namespace Movies.API.GraphQL.Types
 {
     public class MovieType : ObjectGraphType<Movie>
     {
-        public MovieType()
+        public MovieType(MoviesDbContext db)
         {
             Field(a=>a.Id).Description("Id of movie");
             Field(a=>a.Name).Description("Name of movie");
             Field(a=>a.Description).Description("Description of movie");
             Field(a=>a.LaunchDate).Description("Launch date of movie");
             Field<MovieGenreType>("genre").Description("Genre of movie");
-            Field<MovieReviewType>("review").Description("Reviews of movie");
+            Field<ListGraphType<MovieReviewType>>("reviews").Description("Reviews of movie")
+                .ResolveAsync(async context =>
+                {
+                    var movieId = context.Source.Id;
+                    return await db.Reviews.Where(a => a.MovieId == movieId).ToListAsync();
+                });
         }
     }
 }
