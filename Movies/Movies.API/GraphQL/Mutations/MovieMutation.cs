cc20b087ae40b7b72a1c9be97c5db35edcb70b14
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-03-06 21:20:31 -0300

    S06E02 - Mutação concluída

--- Movies/Movies.API/GraphQL/Mutations/MovieMutation.cs
+++ Movies/Movies.API/GraphQL/Mutations/MovieMutation.cs
@@ -1,4 +1,5 @@
-﻿using Movies.API.Data;
+﻿using GraphQL;
+using Movies.API.Data;
 using Movies.API.GraphQL.Types;
 using Movies.API.GraphQL.Types.Inputs;
 
@@ -11,28 +12,58 @@ namespace Movies.API.GraphQL.Mutations
             Field<MovieType>("addMovie").Arguments(
                 new QueryArgument<NonNullGraphType<MovieInputType>>()
                 {
-                    Name = "Movie",
+                    Name = "movie",
                     Description = "Movie input parameter"
                 }
-            );
+            )
+            .ResolveAsync(async context => {
+                var movie = context.GetArgument<Movie>("movie");
+                
+                db.Movies.Add(movie);
+                await db.SaveChangesAsync();
+
+                return movie;
+            });
             Field<MovieType>("updateMovie").Arguments(
                 new QueryArgument<NonNullGraphType<IdGraphType>>() { 
-                    Name = "Id",
+                    Name = "id",
                     Description = "Id input parameter"
                 },
                 new QueryArgument<NonNullGraphType<MovieInputType>>()
                 {
-                    Name = "Movie",
+                    Name = "movie",
                     Description = "Movie input parameter"
                 }
-            );
+            )
+            .ResolveAsync(async context => {
+                var id = context.GetArgument<int>("id");
+                var movie = context.GetArgument<Movie>("movie");
+                movie.Id = id;
+
+                db.Movies.Update(movie);
+                await db.SaveChangesAsync();
+
+                return movie;
+            });
             Field<BooleanGraphType>("deleteMovie").Arguments(
                 new QueryArgument<NonNullGraphType<IdGraphType>>()
                 {
-                    Name = "Id",
+                    Name = "id",
                     Description = "Id input parameter"
                 }
-            );
+            ).ResolveAsync(async context => {
+                var id = context.GetArgument<int>("id");
+
+                var movie = await db.Movies.FindAsync(id);
+                if(movie is not null) { 
+                    db.Movies.Remove(movie);
+                    await db.SaveChangesAsync();
+
+                    return true;
+                }
+
+                return false;
+            });
         }
     }
 }
