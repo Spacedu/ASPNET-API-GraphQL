aa8eaf1c7c1cd138b421b305618ac12d26b4a109
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-03-07 11:47:20 -0300

    S07E08 - C# - Consumir API - Parte 3 - AddMovie

--- Movies/Movies.Client.Console/Program.cs
+++ Movies/Movies.Client.Console/Program.cs
@@ -1,6 +1,22 @@
 ﻿using Movies.GraphQL.Client.Services;
+using Movies.Models;
 
 var service = new MovieService();
 var movie = await service.GetMovie(51);
 Console.WriteLine($"Movie: {movie.Name}");
+
+
+var movies = await service.GetMovies();
+Console.WriteLine($"Total of movies: {movies.Count}");
+
+var newMovie = new Movie() { 
+    Name = "Film 1", 
+    Description = "Description of film 1", 
+    LaunchDate = DateTime.Now, 
+    Genre = Movies.Models.Enums.MovieGenre.Comedy 
+};
+
+var addedMovie = await service.AddMovies(newMovie);
+Console.WriteLine($"Movie added - Id: {addedMovie.Id} - Name: {addedMovie.Name}");
+
 Console.ReadKey();
--- Movies/Movies.GraphQL.Client/Services/MovieService.cs
+++ Movies/Movies.GraphQL.Client/Services/MovieService.cs
@@ -15,13 +15,36 @@ namespace Movies.GraphQL.Client.Services
         {
             var query = new GraphQLRequest()
             {
-                Query = "{ movie(id:"+id+") { id name description launchDate genre } }"
+                Query = "query GetMovie($movieId: Int!) { movie(id:$movieId) { id name description launchDate genre } }",
+                Variables = new { movieId = id }
             };
 
             var response = await Client.SendQueryAsync<MovieResponse>(query);
 
             return response.Data.Movie;
+        }
+        public async Task<List<Movie>> GetMovies()
+        {
+            var query = new GraphQLRequest()
+            {
+                Query = "{ movies { id name description launchDate genre } }",
+            };
 
+            var response = await Client.SendQueryAsync<MoviesResponse>(query);
+
+            return response.Data.Movies;
+        }
+        public async Task<Movie> AddMovies(Movie movie)
+        {
+            var query = new GraphQLRequest()
+            {
+                Query = "mutation AddMovie($movie:MovieInput!) { movie:addMovie( movie:$movie ) { id name description launchDate genre } }",
+                Variables = new { movie = new { movie.Name, movie.Description, movie.LaunchDate, movie.Genre } }
+            };
+
+            var response = await Client.SendMutationAsync<MovieResponse>(query);
+
+            return response.Data.Movie;
         }
     }
 }
